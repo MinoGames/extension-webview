620f1a038637332c25abec00e4a65be7b631029c
Jean-Denis Boivin <jeandenis.boivin@gmail.com>
2019-06-21 23:49:27 -0400

    Update NDLL

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ if [ -d "native/obj" ]; then
     rm -r native/obj
 fi
 
-(cd project && haxelib run hxcpp Build.xml -Diphoneos -DHXCPP_ARM   -DHXCPP_CLANG -DOBJC_ARC $1)
-(cd project && haxelib run hxcpp Build.xml -Diphoneos -DHXCPP_ARMV7 -DHXCPP_CLANG -DOBJC_ARC $1)
-(cd project && haxelib run hxcpp Build.xml -Diphoneos -DHXCPP_ARM64 -DHXCPP_CLANG -DOBJC_ARC $1)
-(cd project && haxelib run hxcpp Build.xml -Diphonesim -DHXCPP_CPP11 -DHXCPP_CLANG -DOBJC_ARC $1)
-(cd project && haxelib run hxcpp Build.xml -Diphonesim -DHXCPP_CPP11 -DHXCPP_M64 -DHXCPP_CLANG -DOBJC_ARC $1)
+(cd project && haxelib run hxcpp Build.xml -DHXCPP_CPP11 -Diphoneos -DHXCPP_ARM   -DHXCPP_CLANG -DOBJC_ARC $1)
+(cd project && haxelib run hxcpp Build.xml -DHXCPP_CPP11 -Diphoneos -DHXCPP_ARMV7 -DHXCPP_CLANG -DOBJC_ARC $1)
+(cd project && haxelib run hxcpp Build.xml -DHXCPP_CPP11 -Diphoneos -DHXCPP_ARM64 -DHXCPP_CLANG -DOBJC_ARC $1)
+(cd project && haxelib run hxcpp Build.xml -DHXCPP_CPP11 -Diphonesim -DHXCPP_CPP11 -DHXCPP_CLANG -DOBJC_ARC $1)
+(cd project && haxelib run hxcpp Build.xml -DHXCPP_CPP11 -Diphonesim -DHXCPP_CPP11 -DHXCPP_M64 -DHXCPP_CLANG -DOBJC_ARC $1)
 
 if [ -d "native/obj" ]; then
     rm -r native/obj
